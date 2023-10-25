d71e566676a04e924f2df8b1e7ed84fc51485377
Kingkubkub <king33130h@gmail.com>
2023-10-25 23:52:38 +0700

    first commit

---
+++ dist/type1.js
@@ -0,0 +1,16 @@
+"use strict";
+let numberOne = 25;
+function add(number1, number2) {
+    return number1 + number2;
+}
+function sub(number1, number2) {
+    return number1 - number2;
+}
+function showDetial() {
+}
+let dataUser = {
+    username: 'king',
+    password: 1234
+};
+const a = 25;
+console.log(add(a, a));
---
+++ type1.ts
@@ -0,0 +1,30 @@
+
+let numberOne:number = 25
+
+
+function add (number1:number,number2:number):number{
+    return number1 + number2
+}
+
+function sub (number1:number,number2:number):number{
+    return number1 - number2
+}
+
+function showDetial():void{
+    
+}
+
+interface user {
+    username:string;
+    password:number;
+}
+
+let dataUser:user = {
+    username:'king',
+    password:1234
+}
+
+
+
+
+
